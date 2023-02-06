9be055bf36b0a906ccbe86a97c07478f452d9723
marromagu <mromagu2611@g.educaand.es>
2023-02-06 12:26:48 +0100

    Fecha class y Fecha main

---
+++ src/main/java/E1_ModificarFecha/FechaMain.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package E1_ModificarFecha;
+
+/**
+ *
+ * @author mario
+ */
+public class FechaMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*E1.- Modificar la clase Fecha
+    Modificaciones en la clase Fecha:
+    Elimina la variable de clase valida.
+    Añadir el método esBisiesto, que recibirá un entero y devolverá verdadero o falso, según sea bisiesto o no el año pasado por parámetro
+    Elimina el método setValida, sustitúyelo por el método comprobarFecha, de tipo boolean, que comprueba si la fecha es válida: los meses de 30 o 31 días, el mes de febrero según sea bisiesto el año.
+    Añadir el método diaSiguiente que cambia la fecha actual por la del día siguiente. la nueva fecha debe ser correcta.*/
+        
+        
+    }
+
+}
